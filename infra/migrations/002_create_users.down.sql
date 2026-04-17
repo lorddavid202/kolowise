b11a023d948c8619658a2a0cf494945977a5bbe7
lorddavid202 <emeka.intergrity@gmail.com>
2026-04-17 20:18:15 +0100

    stop tracking infra and env files

--- infra/migrations/002_create_users.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS users_email_unique_idx;
-DROP TABLE IF EXISTS users;
--- infra/migrations/003_accounts_transactions.down.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS transactions_account_date_idx;
-DROP INDEX IF EXISTS transactions_user_date_idx;
-DROP TABLE IF EXISTS transactions;
-
-DROP INDEX IF EXISTS accounts_user_id_idx;
-DROP TABLE IF EXISTS accounts;
